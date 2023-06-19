5ee83947972c925644b14988ccbc944ffa771f8b
dmitriev-ilya <dmitriev-ilya@inbox.ru>
2023-06-19 20:42:35 +0500

    dialogflow bot added

---
+++ dialogflow_bot.py
@@ -0,0 +1,39 @@
+import os
+
+from google.cloud import dialogflow
+from dotenv import load_dotenv
+
+
+def detect_intent_texts(project_id, session_id, texts, language_code):
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path(project_id, session_id)
+    print("Session path: {}\n".format(session))
+
+    for text in texts:
+        text_input = dialogflow.TextInput(text=text, language_code=language_code)
+
+        query_input = dialogflow.QueryInput(text=text_input)
+
+        response = session_client.detect_intent(
+            request={"session": session, "query_input": query_input}
+        )
+
+        print("=" * 20)
+        print("Query text: {}".format(response.query_result.query_text))
+        print(
+            "Detected intent: {} (confidence: {})\n".format(
+                response.query_result.intent.display_name,
+                response.query_result.intent_detection_confidence,
+            )
+        )
+        print("Fulfillment text: {}\n".format(response.query_result.fulfillment_text))
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    gcloud_project_id = os.environ['GCLOUD_PROJECT_ID']
+    session_id = os.environ['TELEGRAM_USER_ID']
+    texts = ['хай']
+
+    detect_intent_texts(gcloud_project_id, session_id, texts, 'ru')
