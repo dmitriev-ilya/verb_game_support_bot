b9a10fbe57bd5656b72b061f4cd0f2aa714d319b
dmitriev-ilya <dmitriev-ilya@inbox.ru>
2023-06-20 20:58:38 +0500

    combined two bots

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
 import os
+from functools import partial
 
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 from dotenv import load_dotenv
+from google.cloud import dialogflow
 
 
 def start(update: Update, context: CallbackContext):
@@ -12,19 +14,41 @@ def start(update: Update, context: CallbackContext):
     )
 
 
-def echo(update: Update, context: CallbackContext):
-    update.message.reply_text(update.message.text)
+def get_dialogflow_response_text(project_id, session_id, text, language_code):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+    return response.query_result.fulfillment_text
+
+
+def send_reply(update: Update, context: CallbackContext, project_id, session_id):
+    update.message.reply_text(
+        get_dialogflow_response_text(project_id, session_id, update.message.text, 'ru')
+    )
 
 
 if __name__ == '__main__':
     load_dotenv()
     telegram_bot_token = os.environ['SUPPORT_BOT_TELEGRAM_TOKEN']
+    gcloud_project_id = os.environ['GCLOUD_PROJECT_ID']
+    session_id = os.environ['TELEGRAM_USER_ID']
 
     updater = Updater(telegram_bot_token)
 
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dispatcher.add_handler(
+        MessageHandler(
+            Filters.text & ~Filters.command,
+            partial(send_reply, project_id=gcloud_project_id, session_id=session_id)
+        )
+    )
 
     updater.start_polling()
     updater.idle()
