da9d406e9ab0338e08332e835a644eca762d00d2
dmitriev-ilya <dmitriev-ilya@inbox.ru>
2023-06-21 19:56:24 +0500

    add DialogFlow learning script.

--- dialogflow_bot.py
+++ dialogflow_bot.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 from google.cloud import dialogflow
 from dotenv import load_dotenv
@@ -17,7 +18,40 @@ def get_dialogflow_response_text(project_id, session_id, text, language_code):
     return response.query_result.fulfillment_text
 
 
+def create_intent(project_id, display_name, training_phrases_parts, message_texts):
+
+    intents_client = dialogflow.IntentsClient()
+
+    parent = dialogflow.AgentsClient.agent_path(project_id)
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = dialogflow.Intent.TrainingPhrase.Part(text=training_phrases_part)
+        training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+
+    text = dialogflow.Intent.Message.Text(text=message_texts)
+    message = dialogflow.Intent.Message(text=text)
+
+    intent = dialogflow.Intent(
+        display_name=display_name, training_phrases=training_phrases, messages=[message]
+    )
+
+    response = intents_client.create_intent(
+        request={"parent": parent, "intent": intent}
+    )
+
+    print("Intent created: {}".format(response))
+
+
 if __name__ == '__main__':
     load_dotenv()
     gcloud_project_id = os.environ['GCLOUD_PROJECT_ID']
     session_id = os.environ['TELEGRAM_USER_ID']
+
+    with open("questions.json", "r") as my_file:
+        questions_json = my_file.read()
+
+    questions = json.loads(questions_json)
+
+    for intent_name, context in questions.items():
+        create_intent(gcloud_project_id, intent_name, context['questions'], [context['answer'], ])
