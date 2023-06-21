42f6458739107ae35fb6104207e7f58fa5dc02c4
dmitriev-ilya <dmitriev-ilya@inbox.ru>
2023-06-21 20:10:47 +0500

    add VK bot.

---
+++ vk_bot.py
@@ -0,0 +1,23 @@
+import os
+
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+from dotenv import load_dotenv
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    vk_group_api_token = os.environ['VK_GROUP_API_TOKEN']
+
+    vk_session = vk_api.VkApi(token=vk_group_api_token)
+
+    longpoll = VkLongPoll(vk_session)
+
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW:
+            print('Новое сообщение:')
+            if event.to_me:
+                print('Для меня от: ', event.user_id)
+            else:
+                print('От меня для: ', event.user_id)
+            print('Текст:', event.text)
