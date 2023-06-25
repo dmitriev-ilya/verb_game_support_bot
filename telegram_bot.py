cd4ce04ed59bd4fd603dd4e7e65ade4395da8c73
dmitriev-ilya <dmitriev-ilya@inbox.ru>
2023-06-25 13:13:16 +0500

    tg_bot update

--- telegram_bot.py
+++ telegram_bot.py
@@ -6,7 +6,6 @@ from time import sleep
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 from dotenv import load_dotenv
-import telegram
 
 from dialogflow_bot import get_dialogflow_response
 from logger import SupportBotLogsHandler
@@ -16,29 +15,21 @@ logger = logging.getLogger(__file__)
 
 
 def start(update: Update, context: CallbackContext):
-    try:
-        user = update.effective_user
-        update.message.reply_markdown_v2(
-            fr'Здравствуй, {user.mention_markdown_v2()}\!'
-        )
-    except Exception as err:
-        logger.error('Telegram-бот упал с ошибкой:')
-        logger.error(err, exc_info=True)
+    user = update.effective_user
+    update.message.reply_markdown_v2(
+        fr'Здравствуй, {user.mention_markdown_v2()}\!'
+    )
 
 
 def send_reply(update: Update, context: CallbackContext, project_id, session_id):
-    try:
-        dialogflow_response = get_dialogflow_response(
-            project_id,
-            session_id,
-            update.message.text
-        )
-        update.message.reply_text(
-            dialogflow_response.query_result.fulfillment_text
-        )
-    except Exception as err:
-        logger.error('Telegram-бот упал с ошибкой:')
-        logger.error(err, exc_info=True)
+    dialogflow_response = get_dialogflow_response(
+        project_id,
+        session_id,
+        update.message.text
+    )
+    update.message.reply_text(
+        dialogflow_response.query_result.fulfillment_text
+    )
 
 
 if __name__ == '__main__':
@@ -50,18 +41,22 @@ if __name__ == '__main__':
     logger.setLevel(logging.INFO)
     logger.addHandler(SupportBotLogsHandler(telegram_bot_token, session_id))
 
-    updater = Updater(telegram_bot_token)
+    try:
+        updater = Updater(telegram_bot_token)
 
-    dispatcher = updater.dispatcher
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(
-        MessageHandler(
-            Filters.text & ~Filters.command,
-            partial(send_reply, project_id=gcloud_project_id, session_id=session_id)
+        dispatcher = updater.dispatcher
+        dispatcher.add_handler(CommandHandler("start", start))
+        dispatcher.add_handler(
+            MessageHandler(
+                Filters.text & ~Filters.command,
+                partial(send_reply, project_id=gcloud_project_id, session_id=session_id)
+            )
         )
-    )
 
-    updater.start_polling()
-    logger.info('Telegram-бот запущен')
+        updater.start_polling()
+        logger.info('Telegram-бот запущен')
+    except Exception as err:
+        logger.error('Telegram-бот упал с ошибкой:')
+        logger.error(err, exc_info=True)
 
     updater.idle()
